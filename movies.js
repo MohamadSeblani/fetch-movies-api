b3ec17b94e100dc5b9b8c1d8b1886fd85565f19b
MohamadSeblani <mohammad_seblani@live.com>
2023-10-28 22:48:34 +0300

    Enhancements

--- movies.js
+++ movies.js
@@ -9,9 +9,17 @@ fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=a782cb065db909eb
          let card = document.createElement('div')
          card.classList.add('card');
 
+         let youtube = 'https://www.youtube.com/results?search_query=';
+         let search = youtube.concat(movie.original_title.replaceAll(' ', '+')).replaceAll("'", '');
+
          let info = document.createElement('div')
-         info.innerHTML = `<p>${movie.original_title} <br> ${movie.release_date}</p>`;
-         card.appendChild(info)
+         info.innerHTML = `<div class = "titlediv"><h1>${movie.original_title}</h1></div>
+         <div><button onclick="window.location.href='${search}'" class="button">Watch Trailer</button></div>
+         <br><br><br>
+         <p> Release Date: ${movie.release_date} <br> Original Language: ${movie.original_language} <br><br> Popularity: ${movie.popularity} <br> Vote Average: ${movie.vote_average}
+         <br> Vote Count: ${movie.vote_count}</p>`;
+
+         card.appendChild(info);
          info.classList.add('info');
          info.style.display = 'none';
 
@@ -33,4 +41,3 @@ fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=a782cb065db909eb
          container.appendChild(card);
       });
    });
-   // i added fetch a new api
